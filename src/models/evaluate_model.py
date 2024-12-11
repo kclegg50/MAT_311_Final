4445fba0aa8532b60f2d0cbe113b683802c05bbc
kclegg50 <158612284+kclegg50@users.noreply.github.com>
2024-12-10 20:17:06 -0500

    Update evaluate_model.py

--- src/models/evaluate_model.py
+++ src/models/evaluate_model.py
@@ -1,1 +1,4 @@
-
+X = adult_complete.drop(' >50K', axis=1)
+y = adult_complete[' >50K']
+X_train_val, X_test, y_train_val, y_test = train_test_split(X, y, test_size=0.1, random_state=42, stratify=y)
+X_train, X_val, y_train, y_val = train_test_split(X_train_val, y_train_val, test_size=0.2222, random_state=42)
